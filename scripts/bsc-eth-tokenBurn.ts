a9b6692bfc68d51d5e615d543665414add82ecc1
Nick Novoa <nicknovoa03@gmail.com>
2023-06-08 14:50:36 -0500

    add approve

--- scripts/bsc-eth-tokenBurn.ts
+++ scripts/bsc-eth-tokenBurn.ts
@@ -1,5 +1,6 @@
 import { ethers } from 'hardhat';
 import BscBridge from '../artifacts/contracts/BridgeBsc.sol/BridgeBsc.json';
+import BscTruthToken from '../artifacts/contracts/TokenBsc.sol/TokenBsc.json'
 
 async function main() {
   // Connect to the deployed contract instance
@@ -8,16 +9,29 @@ async function main() {
   const [signer] = await ethers.getSigners();
   console.log('Signer:', signer.address);
 
-  // Load contract
-  const contractAddress = '0x8B263fB06dD0e97F6c75992B92f4D2b95e247C9D';
-  const TruthGptBridge = new ethers.Contract(contractAddress, BscBridge.abi, signer);
+  //Get the recievers address
+  const to = '0xAf6C5fF92c0a3F187b063500D47dd1DBf034dC45'
+
+  // Load Bsc Truth Token
+  const bscTruthTokenAddress = '0xB83cA21FED7054bAE76613cEd0215FaA06706361'
+  const BscTruthTokenContract = new ethers.Contract(bscTruthTokenAddress, BscTruthToken.abi, signer)
+
+  // Load Bsc Bridge contract
+  const bscBrigdeContractAddress = '0xaA090c176DBcA6D8b46246fB21950D6E1f53351A';
+  const TruthGptBridgeContract = new ethers.Contract(bscBrigdeContractAddress, BscBridge.abi, signer);
 
   // Set Amount to burn of BscTruth
-  const amount = ethers.utils.parseEther('1')
+  const amount = ethers.utils.parseUnits('1')
+  console.log("amount to burn:", amount)
 
+  console.log('Approving')
+  // Approve Bsc Bridge to spend callers Truth
+  const approveTx = await BscTruthTokenContract.approve(bscBrigdeContractAddress, amount)
+  //Wait for the transaction to be mined
+  await approveTx.wait();
+  console.log('Burning')
   // Call the contract function to write to the contract
-  const tx = await TruthGptBridge.burnToken(amount);
-
+  const tx = await TruthGptBridgeContract.tokenBurn(to, amount);
   //Wait for the transaction to be mined
   await tx.wait();
 }
