83010bef5f1f917c813ca99eaf9851ab8243dfc4
Nick Novoa <nicknovoa03@gmail.com>
2023-06-19 16:15:24 -0500

    set burn address function

--- contracts/BridgeBsc.sol
+++ contracts/BridgeBsc.sol
@@ -5,10 +5,20 @@ import './BridgeBase.sol';
 
 contract BridgeBsc is BridgeBase {
   address public burnAddress = 0x000000000000000000000000000000000000dEaD;
+  bool public bridgeActive = false;
 
   constructor(address token) BridgeBase(token) {}
 
+  function setBurnAddress(address _burnAddress) external onlyOwner {
+    burnAddress = _burnAddress;
+  }
+
+  function setBridgeActive() external onlyOwner {
+    bridgeActive = !bridgeActive;
+  }
+
   function tokenBurn(address to, uint amount) external payable nonReentrant {
+    require(bridgeActive, 'Bridge is not currently active');
     require(token.balanceOf(msg.sender) >= amount, 'Insufficent Balance');
     bool success = token.transferFrom(msg.sender, burnAddress, amount);
     require(success, 'Token transfer from user failed');
